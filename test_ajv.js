cc066a6470bab65d513d1ae0f2806601b7451d5b
plocket <52798256+plocket@users.noreply.github.com>
2023-11-25 21:19:05 -0500

    Use schema v2020, install chai

--- test_ajv.js
+++ test_ajv.js
@@ -1,13 +1,17 @@
 const fs = require('fs');
 const yaml = require('js-yaml');
-const Ajv = require(`ajv`);
+const Ajv = require(`ajv/dist/2020`);
+// const { globSync } = require(`glob`);
 
 // Ours
 const schema = require(`./docassemble_schema.json`);
 // const data = require(`./tests/valid/action_button/code.yml`);
 
+// // Access files in loop with glob
+// const valid_files = globSync(`./valid/**/*.yml`);
+// const invalid_files = globSync(`./invalid/**/*.yml`);
 
-let data = yaml.load(fs.readFileSync('./tests/valid/action_button/array.yml', 'utf8'));
+const data = yaml.load(fs.readFileSync('./tests/valid/action_button/array.yml', 'utf8'));
 
 // Maybe we keep strict mode? https://ajv.js.org/strict-mode.html
 const ajv = new Ajv();
@@ -18,3 +22,5 @@ console.log(`valid:`, valid);
 if (!valid) {
 	console.log(validate.errors);
 }
+
+// Get list of errors with useful details
