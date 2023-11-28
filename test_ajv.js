9bef191fab44b238a566a10f858cd5032f40e27d
plocket <52798256+plocket@users.noreply.github.com>
2023-11-28 14:56:40 -0500

    Add error-only output, outcome summaries, test end notification

--- test_ajv.js
+++ test_ajv.js
@@ -2,76 +2,131 @@
 
 // TODO: dependentRequired, dependentSchemas, unevaluatedProperties
 
+// Node
+const fs = require(`fs`);
+const { exec } = require(`child_process`);
+// Other packages
 const { globSync } = require(`glob`);
-const fs = require('fs');
-const yaml = require('js-yaml');
+const yaml = require(`js-yaml`);
 const Ajv = require(`ajv/dist/2020`);
-const { AggregateAjvError } = require(`@segment/ajv-human-errors`);
-const { default: betterAjvErrors } = require('better-ajv-errors');
-const chai = require(`chai`);
+// const { AggregateAjvError } = require(`@segment/ajv-human-errors`);
+const { default: betterAjvErrors } = require(`better-ajv-errors`);
+// const chai = require(`chai`);
 // Ours
 const schema = require(`./docassemble_schema.json`);
 
 
+// TODO: Maybe loop collecting successes and failures into their own
+// groups (both for just valid, then both for just invalid) and print
+// in those groups? What if the tests actually exit unexpectedly in
+// the middle? All that info would be lost.
+
 // Developer command line arguments/flags
 const dev_args = require(`minimist`)( process.argv.slice(2), {
-  alias: { verbose: `v` },
-  default: { verbose: `false` }
+  alias: {
+    verbose: `v`,
+    "errors-only": `e`,
+  },
+  default: {
+    verbose: `false`,
+    "errors-only": `false`,
+  }
 });
-
 // Schema validator.
 // We're keeping strict mode: https://ajv.js.org/strict-mode.html
 const ajv = new Ajv({
   allErrors: true,  // For ajv-human-errors
   verbose: true,  // For ajv-human-errors
 });
-
-// Create function that will validate the given data
+// Get the function that will validate the YAML blocks
 const validate = ajv.compile(schema);
 
-// TODO: Maybe loop collecting successes and failures into their own
-// groups (both for valid, then both for invalid) and print all at
-// once? What if the tests actually exit in the middle?
 
-console.log(stylize({ message: '--- Positive tests (these YAML blocks should pass) ---', modifier: `important` }));
-
+console.log(stylize({ message: '--- Positive tests (these YAML blocks should pass) ---', styles: `important` }));
 // Confirm these tests pass
 const passer_paths = globSync(`./tests/valid/*/*.yml`);
+const num_passers = passer_paths.length;
+let num_passers_failing = 0;
+
 for ( let file_path of passer_paths ) {
   const { passed, output_body } = run_test({ schema, file_path, validate, dev_args })
   const heading = get_msg_heading({ file_path, passed });
-  console.log( heading + output_body );
+  if ( !passed ) { num_passers_failing += 1; }
+  // Only log errors if that's what dev wants
+  if ( dev_args[`errors-only`] === `true` ) {
+    if ( !passed ) {
+      console.log( heading + output_body );
+    }
+  // Otherwise log every message
+  } else {
+    console.log( heading + output_body );
+  }
 }
 
-console.log(stylize({ message: '\n--- Negative tests (these YAML blocks should fail) ---', modifier: `important` }));
+const passers_totals_msg = get_totals_msg({
+  total_tests: num_passers,
+  num_unexpected: num_passers_failing,
+  msg_end: `positive tests`
+});
+console.log(passers_totals_msg);
 
+
+console.log(stylize({ message: '\n--- Negative tests (these YAML blocks should fail) ---', styles: `important` }));
 // Confirm these tests fail
 const failer_paths = globSync(`./tests/invalid/*/*.yml`);
+const num_failers = failer_paths.length;
+let num_failers_passing = 0;
+
 for ( let file_path of failer_paths ) {
-  const { passed, output_body } = run_test({ schema, file_path, validate, dev_args })
-  const heading = get_msg_heading({ file_path, passed: !passed });
-  console.log( heading + output_body );
+  const { passed, output_body } = run_test({ schema, file_path, validate, dev_args });
+  // TODO: Also fail with unexpected error
+  const met_expectations = !passed;
+  const heading = get_msg_heading({ file_path, passed: met_expectations });
+  if ( !met_expectations ) { num_failers_passing += 1; }
+  // Only log errors if that's what dev wants
+  if ( dev_args[`errors-only`] === `true` ) {
+    if ( passed ) {
+      console.log( heading + output_body );
+    }
+  // Otherwise log every message
+  } else {
+    console.log( heading + output_body );
+  }
 }
 
-// // // Get list of errors with useful details
-// // if (!valid) {
-// //   // TODO: figure out which "path" is the longest and just print that
-// //   // one. Or at least print that one first.
-// //   console.error(stylize(`------------------------`, `error`));
-// //   console.error(stylize({message: `Failed:`, modifier: `error`}), file_path);
-// //   const errors = new AggregateAjvError(validate.errors);
-// //   console.log(errors.message.replace(/\. /g, `.\n`));
-// //   // console.log(validate.errors);
-// // } else {
-// //   console.log(stylize(`Passed:`, `success`), file_path);
-// // }
+const failers_totals_msg = get_totals_msg({
+  total_tests: num_failers,
+  num_unexpected: num_failers_passing,
+  msg_end: `negative tests`
+});
+console.log(failers_totals_msg);
+
+const all_totals_msg = get_totals_msg({
+  total_tests: num_passers + num_failers,
+  num_unexpected: num_passers_failing + num_failers_passing,
+  msg_end: `in total`
+});
+console.log(`\n============================\nSummary`);
+console.log(all_totals_msg);
+
+// The oven is done baking
+process.stdout.write(`\u0007`);
+// Neither of these made sounds on OSX, but maybe they'll work on other systems
+exec(`[console]::beep(1000, 500)`, {'shell':`powershell.exe`});
+exec(`rundll32 user32.dll, MessageBeep`);
+// TODO: use a lib to get a more pleasant sound
+
+
+// ================================
+// ================================
+// === Helpers ===
 
 function run_test ({ schema, file_path, validate, dev_args }) {
   // Not sure validate is needed, maybe it can be global
-  const data = yaml.load( fs.readFileSync( file_path, `utf8` ));
-  const passed = validate(data);
-  const options = get_options({ data, dev_args });
-  const unformatted_output = betterAjvErrors(schema, data, validate.errors, options);
+  const blocks = yaml.load( fs.readFileSync( file_path, `utf8` ));
+  const passed = validate(blocks);
+  const options = get_options({ blocks, dev_args });
+  const unformatted_output = betterAjvErrors(schema, blocks, validate.errors, options);
   const output_body = format_BAE_error_bodies({
     output: unformatted_output,
     dev_args,
@@ -80,7 +135,7 @@ function run_test ({ schema, file_path, validate, dev_args }) {
   return { passed, output_body };
 }
 
-function get_options ({ data, dev_args }) {
+function get_options ({ blocks, dev_args }) {
   // If verbose, make sure the json we output is pretty
   if ( dev_args.verbose === `true` ) {
     return { indent: 2 }
@@ -91,7 +146,7 @@ function get_options ({ data, dev_args }) {
     // Make sure we get the right line numbers by changing
     // the json string. This only works if any yaml string
     // are on the same line as their key. (No `code: |`)
-    json: JSON.stringify(data, null, 2)
+    json: JSON.stringify(blocks, null, 2)
           .replace( /\n\s*\{/g, `{` )
           .replace( /\n\s*\[/g, `[` )
           .replace( /\n\s*}/g, `}` )
@@ -106,11 +161,11 @@ function get_options ({ data, dev_args }) {
 function get_msg_heading ({ file_path, passed }) {
   /** Get the separator and heading of a failing test. */
   if ( passed ) {
-    return `${ stylize({ message: `Passed:`, modifier: `success` }) } ${ file_path }`;
+    return `${ stylize({ message: `Passed:`, styles: `success` }) } ${ file_path }`;
   } else {
     return (
-      stylize({ message: `------------------------`, modifier: `error` })
-      + `\n${ stylize({ message: `Failed:`, modifier: `error` }) } in file ${ file_path }`
+      stylize({ message: `------------------------`, styles: `error` })
+      + `\n${ stylize({ message: `Failed:`, styles: `error` }) } in file ${ file_path }`
     )
   }
 }
@@ -146,13 +201,13 @@ function get_BAE_error_msg_body ({ error_data, verbose }) {
   // Make suggestion bold if there is one.
   let suggestion = error_data.suggestion || '';
   if ( suggestion ) {
-    suggestion = " " + stylize({ message: suggestion, modifier: `important` })
+    suggestion = " " + stylize({ message: suggestion, styles: `important` })
   }
   return (
     `\n${ error_data.error }.${ suggestion }`
     // Line numbers are only useful when yaml string are on the
     // same line as their key
-    + `\n` + stylize({ message: `Line ${ error_data.start.line }`, modifier: `aside` })
+    + `\n` + stylize({ message: `Line ${ error_data.start.line }`, styles: `aside` })
     // + `\n\x1b[90mLine ${ error_data.start.line }\x1b[0m`
   );
 }
@@ -164,7 +219,24 @@ function get_BAE_error_msg_body ({ error_data, verbose }) {
 //   return error_data.message.replace(/\. /g, `.\n`);
 // }
 
-function stylize ({ message, modifier }) {
+function get_totals_msg ({ total_tests, num_unexpected, msg_end }) {
+  /** Returns the message to show at the end of the tests */
+  if ( num_unexpected > 0 ) {
+    return (
+      stylize({
+        message: `--------------\n${ num_unexpected }/${ total_tests } failing ${ msg_end }\n--------------`,
+        styles: `error` })
+    )
+  } else {
+    return (
+      stylize({
+        message: `--------------\n${ total_tests }/${ total_tests } passing ${ msg_end }\n--------------`,
+        styles: `success` })
+    )
+  }
+}
+
+function stylize ({ message, styles }) {
   /** Add styles to a message that will be put in the command prompt
    *  or browser console.
    *
@@ -172,20 +244,18 @@ function stylize ({ message, modifier }) {
    * See https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
    *
    * @param message {str}
-   * @param modifier {str} A string of the type of message
+   * @param styles {str} A string of the type of message
    *  (`error`, `success`, etc.) or a style string itself. */
-
-  let styles = modifier;
-  if ( modifier === `error` ) {
+  if ( styles === `error` ) {
     /* styles: */ `bold \x1b[1m, fg red \x1b[31m`
     styles = `\x1b[1m\x1b[31m`;
-  } else if ( modifier === `success` ) {
+  } else if ( styles === `success` ) {
     /* styles: */ `fg green \x1b[32m`
     styles = `\x1b[32m`;
-  } else if ( modifier === `important` ) {
+  } else if ( styles === `important` ) {
     /* styles: */ `bold \x1b[1m`
     styles = `\x1b[1m`;
-  } else if ( modifier === `aside` ) {
+  } else if ( styles === `aside` ) {
     /* styles: */ `fg gray \x1b[90m`
     styles = `\x1b[90m`;
   }
